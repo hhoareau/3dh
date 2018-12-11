4493e8c2525712228f035b074663301ed30c38b6
hhoareau <hhoareau@gmail.com>
2018-12-11 15:30:00 +0100

    déployment vers netlify

--- src/http-constants.ts
+++ src/http-constants.ts
@@ -3,10 +3,10 @@ import axios from 'axios'
 let baseURL;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    baseURL = 'http://localhost:5000/'
+    baseURL = 'https://localhost:5000/'
     //baseURL = "http://45.77.160.220:5000"
 } else {
-    baseURL = 'http://45.77.160.220:5000/'
+    baseURL = 'https://ss.shifumix.com:5000/'
 }
 
 export const HTTP = axios.create({
