a77dbd88ddf2cd36ed4174a94a0eac02752505cf
hhoareau <hhoareau@gmail.com>
2018-12-11 10:40:07 +0100

    déployment sur les github page

---
+++ docs/precache-manifest.ded9b771b7fbd10a2922c80b5d257e5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1db0db1ccf8c89ab112b0515c20a3ef",
+    "url": "/waiting.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a15dba6f1c64df078c6a",
+    "url": "/js/chunk-vendors.4aa1ff94.js"
+  },
+  {
+    "revision": "cdac1ba0e973433f9a73",
+    "url": "/js/app.ea8ed498.js"
+  },
+  {
+    "revision": "0c8ebb40728e8de0a6d627bc7ba34490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80155d26ad13311bbb09a09d0e74abf",
+    "url": "/help.html"
+  },
+  {
+    "revision": "a15dba6f1c64df078c6a",
+    "url": "/css/chunk-vendors.85f1427b.css"
+  },
+  {
+    "revision": "cdac1ba0e973433f9a73",
+    "url": "/css/app.f36eafb0.css"
+  }
+];
