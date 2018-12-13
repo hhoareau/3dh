1538347c17cebdba3399be37d5502e3fb8a31e62
shifumix <dev@shifumix.com>
2018-12-13 20:49:33 +0100

    déployment vers netlify

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/3dh/precache-manifest.4f49c5fc94c5cc1e72f959f3922908b8.js"
+  "/3dh/precache-manifest.82dbcad8c8201556e8918231eed7ff6e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "3dh"});
--- src/http-constants.ts
+++ src/http-constants.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 let baseURL;
-
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:5000/'
 } else {
