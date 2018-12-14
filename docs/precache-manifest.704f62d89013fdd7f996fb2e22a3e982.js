81c99ad8f948cacf9c24f8db67a3694e688f6a5a
shifumix <dev@shifumix.com>
2018-12-14 10:58:08 +0100

    déployment vers netlify

---
+++ docs/precache-manifest.704f62d89013fdd7f996fb2e22a3e982.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1db0db1ccf8c89ab112b0515c20a3ef",
+    "url": "/waiting.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a15dba6f1c64df078c6a",
+    "url": "/js/chunk-vendors.4aa1ff94.js"
+  },
+  {
+    "revision": "ad945fd58e0c70a43c37",
+    "url": "/js/app.b5a61fb0.js"
+  },
+  {
+    "revision": "7b37e2a614e7c6ad48e1af4b4e0ddd62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80155d26ad13311bbb09a09d0e74abf",
+    "url": "/help.html"
+  },
+  {
+    "revision": "a15dba6f1c64df078c6a",
+    "url": "/css/chunk-vendors.85f1427b.css"
+  },
+  {
+    "revision": "ad945fd58e0c70a43c37",
+    "url": "/css/app.ac9626da.css"
+  }
+];
