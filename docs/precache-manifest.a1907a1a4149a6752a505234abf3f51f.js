90390c994d7a0703d54c36181e2c7323ad313f21
shifumix <dev@shifumix.com>
2018-12-18 14:52:29 +0100

    déployment avec ajout du site complet sur build v2

---
+++ docs/precache-manifest.a1907a1a4149a6752a505234abf3f51f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a829e88a9fc74a00ae94feca5f13d475",
+    "url": "/www/images/600X300.gif"
+  },
+  {
+    "revision": "c0283af20855bdf7af5b168cd963b085",
+    "url": "/www/movies/clusterBench.webm"
+  },
+  {
+    "revision": "801361b301c919e43e0f21c044ba80aa",
+    "url": "/www/start.html"
+  },
+  {
+    "revision": "c17ae4f0529e0bc2fc887d2471ae124b",
+    "url": "/www/css/bootstrap-4.0.0.css"
+  },
+  {
+    "revision": "a15dba6f1c64df078c6a",
+    "url": "/js/chunk-vendors.4aa1ff94.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "72d4c509ae27e655a0fa8dc135364eda",
+    "url": "/www/help.html"
+  },
+  {
+    "revision": "03e1f4355c59af5e1d940869cf4a22a7",
+    "url": "/www/images/1200X400.gif"
+  },
+  {
+    "revision": "d1b0c0d8b60cd2bad3d78c483bdc20ad",
+    "url": "/www/images/35X35.gif"
+  },
+  {
+    "revision": "b1dbc64f8b1dfe0c089dd55b09bbbc72",
+    "url": "/www/js/popper.min.js"
+  },
+  {
+    "revision": "5d657bba8f8f4b71fdfcfdb37dff127a",
+    "url": "/www/images/32X32.gif"
+  },
+  {
+    "revision": "473957cfb255a781b42cb2af51d54a3b",
+    "url": "/www/js/jquery-3.2.1.min.js"
+  },
+  {
+    "revision": "a1db0db1ccf8c89ab112b0515c20a3ef",
+    "url": "/waiting.html"
+  },
+  {
+    "revision": "3d389eae3d97497404315ed822c376df",
+    "url": "/www/images/ImgResponsive_Placeholder.png"
+  },
+  {
+    "revision": "bb38938b1b9056351722c813ddbbbe70",
+    "url": "/www/js/bootstrap-4.0.0.js"
+  },
+  {
+    "revision": "1d339c5fb190b8128484",
+    "url": "/js/app.6aa96576.js"
+  },
+  {
+    "revision": "3776552b8c2099543235ded150c7c023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a15dba6f1c64df078c6a",
+    "url": "/css/chunk-vendors.85f1427b.css"
+  },
+  {
+    "revision": "1d339c5fb190b8128484",
+    "url": "/css/app.b06b67b5.css"
+  }
+];
